bd9c9230358fc31667324cf9d35bed6d9cdf6723
JOSΞ <jflores717@gmail.com>
2025-08-18 13:51:22 -0400

    chore: some more cleanup

--- extensions/actor-spa-vscode/out/commands/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function generateComponent(): Promise<void>;
-export declare function generateMachine(): Promise<void>;
-export declare function validateTemplate(): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
--- extensions/actor-spa-vscode/out/providers/validationProvider.d.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from 'vscode';
-export declare class ActorSpaValidationProvider implements vscode.CodeActionProvider {
-    provideCodeActions(document: vscode.TextDocument, _range: vscode.Range | vscode.Selection, _context: vscode.CodeActionContext, _token: vscode.CancellationToken): vscode.ProviderResult<(vscode.Command | vscode.CodeAction)[]>;
-    private validateTemplate;
-}
-//# sourceMappingURL=validationProvider.d.ts.map
